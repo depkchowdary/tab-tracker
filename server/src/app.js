634aa53e2b976e0083cd6cc8b06be1ead6bf3600
Deepak Tatineni <depkchowdary@outlook.com>
2018-09-11 02:02:37 +0530

    Initial SPA scafolding

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+
+// test endpoint
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(process.env.PORT || 8081, () => {
+  console.log(`Backend server for tab tracker is running`)
+})
